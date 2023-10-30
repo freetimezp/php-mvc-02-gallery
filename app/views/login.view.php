ca44918f5b6ca364af349479f0a46ddf39beb1c5
freetimezp <kraftukrnet@ukr.net>
2023-10-30 18:50:35 +0200

    04. php-mvc-02-gallery. Migration Users, Photos table. Login, Signup views.

--- app/views/login.view.php
+++ app/views/login.view.php
@@ -1,9 +1,15 @@
-<h1>Login page view</h1>
-<form method="post">
-	
-	<input value="<?=old_value('email')?>" name="email" placeHolder="Email"><br>
-	<div><?=$user->getError('email')?></div><br>
-	<input value="<?=old_value('password')?>" name="password" placeHolder="Password"><br>
-	<div><?=$user->getError('password')?></div><br>
-	<button>Login</button>
-</form>
+<?php $this->view('includes/header', $data); ?>
+
+<div class="col-md-4 border bg-light shadow p-4 mt-4 mx-auto">
+	<h1 class="mb-4">Login</h1>
+	<form method="post">
+
+		<input class="form-control" value="<?= old_value('email') ?>" name="email" placeHolder="Email"><br>
+		<div><small class="text-danger"><?= $user->getError('email') ?></small></div><br>
+		<input class="form-control" value="<?= old_value('password') ?>" name="password" placeHolder="Password"><br>
+		<div><small class="text-danger"><?= $user->getError('password') ?></small></div><br>
+		<button class="btn btn-primary">Login</button>
+	</form>
+</div>
+
+<?php $this->view('includes/footer', $data);
--- app/views/signup.view.php
+++ app/views/signup.view.php
@@ -1,11 +1,17 @@
-<h1>Signup page view</h1>
-<form method="post">
-	
-	<input value="<?=old_value('username')?>" name="username" placeHolder="Username"><br>
-	<div><?=$user->getError('username')?></div><br>
-	<input value="<?=old_value('email')?>" name="email" placeHolder="Email"><br>
-	<div><?=$user->getError('email')?></div><br>
-	<input value="<?=old_value('password')?>" name="password" placeHolder="Password"><br>
-	<div><?=$user->getError('password')?></div><br>
-	<button>Signup</button>
-</form>
+<?php $this->view('includes/header', $data); ?>
+
+<div class="col-md-4 border bg-light shadow p-4 mt-4 mx-auto">
+	<h1>Signup</h1>
+
+	<form method="post">
+		<input class="form-control" value="<?= old_value('username') ?>" name="username" placeHolder="Username"><br>
+		<div><small class="text-danger"><?= $user->getError('username') ?></small></div><br>
+		<input class="form-control" value="<?= old_value('email') ?>" name="email" placeHolder="Email"><br>
+		<div><small class="text-danger"><?= $user->getError('email') ?></small></div><br>
+		<input class="form-control" value="<?= old_value('password') ?>" name="password" placeHolder="Password"><br>
+		<div><small class="text-danger"><?= $user->getError('password') ?></small></div><br>
+		<button class="btn btn-primary">Signup</button>
+	</form>
+</div>
+
+<?php $this->view('includes/footer', $data);
